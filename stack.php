672148eaf930663f95ebe9330412f6012c396a25
MINuxboy <minulapriyashan2004@gmail.com>
2024-10-21 11:50:39 +0530

    stack file add

---
+++ stack.php
@@ -0,0 +1,36 @@
+<?php
+class stack{
+    private $_size;
+    private $_stack = [];
+    function __construct($size)
+    {
+        echo "stack\n";
+    }
+
+    public function push($n){
+      if(count($this->_stack) >= $this->_size){
+        return false;
+      }
+      $this->_stack[]=$n;
+       return true;
+    }
+   public function pop($size){
+    $this-> _array_pop($this->_stack);
+
+    }
+    public function top(){
+
+    }
+}
+
+
+// $s1=new stack(15);
+
+for ($i=1; $i <=20; ++$i){
+    $s1->push($i);
+
+}
+
+// while(($val =rs1->pop()) !=null){
+//     echo $val . "\n";
+// }
