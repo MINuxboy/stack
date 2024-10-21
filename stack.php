f96c8b3b02d3f60c004b33f42a091eff022e05d8
MINuxboy <minulapriyashan2004@gmail.com>
2024-10-21 22:07:48 +0530

    add to

--- stack.php
+++ stack.php
@@ -16,15 +16,9 @@ class stack{
 
       $this->_stack[]=$n;
        return true;
-<<<<<<< HEAD
-      }
-       public function pop($size){
-       $this-> _array_pop($this->_stack);
-=======
     }
    public function pop($size){     
     $this-> _array_pop($this->_stack);
->>>>>>> dev
 
       }
         public function top(){
